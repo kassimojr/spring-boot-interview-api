57ec255255cbd2506878bddab9762eb70fe27ff0
Kássimo Junior <kassimojr@outlook.com>
2019-04-13 16:26:04 -0300

    Commit inicial da API a ser desenvolvida

---
+++ src/main/java/com/compasso/springbootinterview/SpringBootInterviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.compasso.springbootinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInterviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInterviewApiApplication.class, args);
+	}
+
+}
